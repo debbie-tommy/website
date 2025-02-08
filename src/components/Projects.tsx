3f160365987ef0df6284744aefe616b999b84c1a
debbie-tommy <68273014+debbie-tommy@users.noreply.github.com>
2025-02-08 21:41:06 +0530

    bug fix

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,6 +1,6 @@
 
 import styled from 'styled-components';
-import  motion  from 'framer-motion';
+import { motion } from 'framer-motion';
 
 const ProjectsSection = styled.section`
   padding: 6rem 4rem;
