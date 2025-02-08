a427d91c7d4f83c1924fe1a4c742fd0df5e2a2a8
debbie-tommy <68273014+debbie-tommy@users.noreply.github.com>
2025-02-08 18:42:15 +0530

    changes to file directory

--- portfolio/src/components/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as CustomCursor } from './CustomCursor';
-export { default as Navigation } from './Navigation';
-export { default as Hero } from './Hero';
-export { default as Skills } from './Skills';
-export { default as Projects } from './Projects';
-export { default as LoadingScreen } from './LoadingScreen';
